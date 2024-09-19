6839e1bac2b348c3aba9979d5fc9f2838e964612
Hannah Koenker <63600385+hkoenker@users.noreply.github.com>
2024-09-19 14:48:18 -0400

    ignore file copy script

---
+++ Copy Caps Excel Sheet.R
@@ -0,0 +1,3 @@
+file.copy(from="/Users/hannahkoenker/Dropbox/A DHS MIS Datasets/Analysis/Caps/caps_regional_hhsizes.xlsx", to="/Users/hannahkoenker/Documents/GitHub/Caps/", 
+          overwrite = TRUE, recursive = FALSE, 
+          copy.mode = TRUE)
